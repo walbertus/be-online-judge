7e33c67abd9fd9b8f42177563ebe96824e634896
William AD <w.albertusd@gmail.com>
2018-07-09 18:46:50 +0700

    Create auth login success test

--- tests/Feature/Api/V1/AuthControllerTest.php
+++ tests/Feature/Api/V1/AuthControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Api\V1;
 
 use App\Api\V1\Domain\User\Entity\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestResponse;
 use Tests\FeatureTestCase;
 
 class AuthControllerTest extends FeatureTestCase
@@ -20,15 +21,21 @@ class AuthControllerTest extends FeatureTestCase
     const USER_PASSWORD_SHORT = 'sec';
 
     const URI_REGISTER = 'api/auth/register';
+    const URI_LOGIN = 'api/auth/login';
 
     public function testRegisterSuccess(): void
     {
-        $respone = $this->call('POST', self::URI_REGISTER, [
+        $response = $this->validRegister();
+        $response->assertStatus(201);
+    }
+
+    protected function validRegister(): TestResponse
+    {
+        return $this->call('POST', self::URI_REGISTER, [
             self::PARAM_NAME => self::USER_NAME,
             self::PARAM_EMAIL => self::USER_EMAIL,
             self::PARAM_PASSWORD => self::USER_PASSWORD,
         ]);
-        $respone->assertStatus(201);
     }
 
     public function testRegisterEmailAlreadyExist(): void
@@ -65,6 +72,24 @@ class AuthControllerTest extends FeatureTestCase
         $this->assertArrayHasKey('errors', $jsonResponse);
         $this->assertArrayHasKey(self::PARAM_PASSWORD, $jsonResponse['errors']);
         $this->assertArrayHasKey(self::PARAM_EMAIL, $jsonResponse['errors']);
+    }
+
+    public function testLoginSuccessful()
+    {
+        $this->validRegister();
+        $response = $this->validLogin();
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'access_token', 'token_type', 'expires_in',
+        ]);
+    }
+
+    protected function validLogin(): TestResponse
+    {
+        return $this->call('POST', self::URI_LOGIN, [
+            self::PARAM_EMAIL => self::USER_EMAIL,
+            self::PARAM_PASSWORD => self::USER_PASSWORD,
+        ]);
     }
 
     protected function createUser(): void
