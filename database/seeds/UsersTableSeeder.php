da2b8519e915039ed78bdc9cdc1e1d4e8ec22fe6
William AD <w.albertusd@gmail.com>
2018-07-14 00:18:19 +0700

    Assign role to user seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,13 +2,15 @@
 
 use App\Api\V1\Domain\User\Entity\User;
 use Illuminate\Database\Seeder;
+use Silber\Bouncer\Bouncer;
 
 class UsersTableSeeder extends Seeder
 {
-    public function run(): void
+    public function run(Bouncer $bouncer): void
     {
-        factory(User::class)->create([
+        $user = factory(User::class)->create([
             'email' => 'qweasd@qwe.com',
         ]);
+        $bouncer->assign('problem-setter')->to($user);
     }
 }
